af6321599fe1bf25e5c2e907f5d618f67ed0e4ea
Juhyeon Lee <ljuh0928@gmail.com>
2025-04-04 15:27:23 +0900

    feat: add function using cuda in hw1

--- hw1/HW_1.cu
+++ hw1/HW_1.cu
@@ -1,8 +1,57 @@
 #include <stdio.h>
+#include "cuda_runtime.h"
+#include <curand_kernel.h>
 
 #define rand_uniform ((float)rand() / RAND_MAX)
 
 float HW1_SPHERE_host(int n);
+float HW1_SPHERE_reduce1(int n);
+
+__global__ void init_curand_states(curandState *states, unsigned long seed)
+{
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    // each thread gets a different state
+    curand_init(seed, idx, 0, &states[idx]);
+}
+
+__global__ void do_MonteCarlo_simulation(curandState *states, float *counts, int n)
+{
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+
+    // Check if the thread ID is within bounds
+    if (tid >= n)
+        return;
+
+    // Initialize the count to 0
+    counts[tid] = 0;
+
+    // Get the state for this thread
+    curandState localState = states[tid];
+
+    // Generate a random float between -1.0 and 1.0
+    float x = 2.0f * curand_uniform(&localState) - 1.0f;
+    float y = 2.0f * curand_uniform(&localState) - 1.0f;
+    float z = 2.0f * curand_uniform(&localState) - 1.0f;
+
+    // Check if the generated point is inside the unit sphere
+    if (x * x + y * y + z * z <= 1.0f)
+        counts[tid] = 1;
+
+    // Save the state back: unnecessary unless this kernel is called again
+    states[tid] = localState;
+}
+
+__global__ void reduce1(float *x, int N)
+{
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    float tsum = 0.0f;
+    int stride = gridDim.x * blockDim.x;
+    for (int k = tid; k < N; k += stride)
+    {
+        tsum += x[k];
+    }
+    x[tid] = tsum;
+}
 
 int main()
 {
@@ -11,10 +60,17 @@ int main()
     double unit_sphere_volume_simulated;
 
     unit_sphere_volume_simulated = HW1_SPHERE_host(N);
-    fprintf(stdout, "\nArea of unit sphere: ");
+
+    fprintf(stdout, "\nHW1_SPHERE_host\nArea of unit sphere: ");
     fprintf(stdout, "simulated = %.15f / ", unit_sphere_volume_simulated);
     fprintf(stdout, "exact = %.15f / ", unit_sphere_volume_exact);
-    fprintf(stdout, "relative error = %.15f\n", unit_sphere_volume_simulated);
+    fprintf(stdout, "relative error = %.15f\n", fabs(unit_sphere_volume_simulated - unit_sphere_volume_exact) / unit_sphere_volume_exact);
+
+    unit_sphere_volume_simulated = HW1_SPHERE_reduce1(N);
+    fprintf(stdout, "\nHW1_SPHERE_reduce1\nArea of unit sphere: ");
+    fprintf(stdout, "simulated = %.15f / ", unit_sphere_volume_simulated);
+    fprintf(stdout, "exact = %.15f / ", unit_sphere_volume_exact);
+    fprintf(stdout, "relative error = %.15f\n", fabs(unit_sphere_volume_simulated - unit_sphere_volume_exact) / unit_sphere_volume_exact);
 }
 
 float HW1_SPHERE_host(int n)
@@ -39,4 +95,36 @@ float HW1_SPHERE_host(int n)
 
     // Return the volume of the sphere
     return 8.0 * count / n; // The volume of the unit cube is 8
+}
+
+float HW1_SPHERE_reduce1(int n)
+{
+    int threads = 256;
+    int blocks = n / threads; // assume N is a multiple of thread block size.
+    // unsigned int n_trials_per_thread = 1 << 16;
+    // N * n_trials_per_thread = simulation size n
+
+    curandState *d_states;
+    cudaMalloc(&d_states, n * sizeof(curandState));
+
+    init_curand_states<<<blocks, threads>>>(d_states, time(NULL));
+
+    float *d_counts;
+    cudaMalloc(&d_counts, n * sizeof(double));
+
+    do_MonteCarlo_simulation<<<blocks, threads, threads * sizeof(float)>>>(d_states, d_counts, n);
+    cudaDeviceSynchronize();
+
+    // free(counts);
+
+    // do reduction here
+    reduce1<<<blocks, threads>>>(d_counts, n);
+    reduce1<<<1, threads>>>(d_counts, blocks * threads);
+    reduce1<<<1, 1>>>(d_counts, threads);
+
+    float sum;
+    cudaMemcpy(&sum, d_counts, sizeof(float), cudaMemcpyDeviceToHost);
+
+    float volume = 8.0 * sum / n;
+    return volume;
 }
