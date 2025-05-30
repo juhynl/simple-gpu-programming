140f7f6ba1059a79a163e86cc23a69a45fcff6dd
Juhyeon Lee <ljuh0928@gmail.com>
2025-05-30 14:39:31 +0900

    feat: add time measurement function

---
+++ matrix_multiplication/measure_host_time.h
@@ -0,0 +1,25 @@
+#pragma once
+// /******************************************************************************************************/
+// #include <Windows.h>
+// static __int64 _start, _freq, _end;
+// static float _compute_time;
+// #define CHECK_TIME_START(start, freq)                  \
+//     QueryPerformanceFrequency((LARGE_INTEGER *)&freq); \
+//     QueryPerformanceCounter((LARGE_INTEGER *)&start)
+// #define CHECK_TIME_END(start, end, freq, time)      \
+//     QueryPerformanceCounter((LARGE_INTEGER *)&end); \
+//     time = (float)((float)(end - start) / (freq * 1.0e-3f))
+// /******************************************************************************************************/
+
+/******************************************************************************************************/
+#include <time.h>
+
+static struct timespec _start, _end;
+static float _compute_time;
+
+#define CHECK_TIME_START(start) clock_gettime(CLOCK_MONOTONIC, &start)
+#define CHECK_TIME_END(start, end, time)          \
+    clock_gettime(CLOCK_MONOTONIC, &end);         \
+    time = ((end.tv_sec - start.tv_sec) * 1e3f) + \
+           ((end.tv_nsec - start.tv_nsec) / 1e6f)
+/******************************************************************************************************/
